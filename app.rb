f79a113ee266102d699686cf61a46196edf49f97
github username <github email address>
2021-04-29 20:55:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,10 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+    # or shortened version is 
+    # @analyzed_text = TextAnalyzer.new(params[:user_text]
+binding.pry
     erb :results
   end
 end
